87ea09fcd3ddac9fb66a94da3830f49188e2b37c
Jakub Gmur <jakub@gmur.me>
2020-10-31 19:02:18 +0100

    Use enum values for the title type

--- src/main/kotlin/me/gmur/buddywatch/justwatch/api/Title.kt
+++ src/main/kotlin/me/gmur/buddywatch/justwatch/api/Title.kt
@@ -5,5 +5,14 @@ import com.google.gson.annotations.SerializedName
 class Title(
     private val id: Long,
     private val title: String,
-    @SerializedName("object_type") private val type: String
+    @SerializedName("object_type") private val type: TitleType
 )
+
+enum class TitleType {
+
+    @SerializedName("movie")
+    MOVIE,
+
+    @SerializedName("show")
+    SHOW
+}
