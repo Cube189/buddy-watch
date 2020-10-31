aa53726ba53b3e7538fdc96ed86667b7d0692038
Jakub Gmur <jakub@gmur.me>
2020-10-31 17:33:00 +0100

    Implement adding body to an HTTP request

--- src/main/kotlin/me/gmur/buddywatch/justwatch/api/Http.kt
+++ src/main/kotlin/me/gmur/buddywatch/justwatch/api/Http.kt
@@ -2,10 +2,15 @@ package me.gmur.buddywatch.justwatch.api
 
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
 
-class Http constructor(private val url: String = BASE_URL) {
+class Http constructor(
+    private val url: String = BASE_URL,
+    private val body: String? = null
+) {
 
     companion object {
         const val BASE_URL = "https://apis.justwatch.com/content/"
@@ -14,7 +19,11 @@ class Http constructor(private val url: String = BASE_URL) {
     private val json = Gson()
 
     fun path(path: String): Http {
-        return Http(url + path)
+        return Http(url + path, body)
+    }
+
+    fun body(body: String): Http {
+        return Http(url, body)
     }
 
     fun <T : Any> execute(type: TypeToken<T>): T {
@@ -25,15 +34,23 @@ class Http constructor(private val url: String = BASE_URL) {
 
     fun execute(): String {
         val client = OkHttpClient()
-        val request = Request.Builder()
-            .header("User-Agent", "BuddyWatch")
-            .url(url)
-            .build()
+        val request = request()
 
         val response = client.newCall(request).execute()
 
         if (!response.isSuccessful) throw RequestUnsuccessful(request.url.toString(), response.code)
 
         return response.body!!.string()
+    }
+
+    private fun request(): Request {
+        val request = Request.Builder()
+            .header("User-Agent", "BuddyWatch")
+            .url(url)
+
+        return if (body != null)
+            request.post(body.toRequestBody("application/json".toMediaType())).build()
+        else
+            request.build()
     }
 }
