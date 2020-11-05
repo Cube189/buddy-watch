540b1f5c38d17b70a55406bf82d8257f2d45d8a0
Jakub Gmur <jakub@gmur.me>
2020-11-06 00:17:26 +0100

    Implement the REST API for tokens

---
+++ src/main/kotlin/me/gmur/buddywatch/auth/adapter/rest/TokenEndpoints.kt
@@ -0,0 +1,27 @@
+package me.gmur.buddywatch.auth.adapter.rest
+
+import me.gmur.buddywatch.auth.domain.model.Token
+import me.gmur.buddywatch.auth.domain.service.TokenService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/token")
+class TokenEndpoints(private val service: TokenService) {
+
+    @PostMapping
+    fun register(): ResponseEntity<Token> {
+        return ResponseEntity(service.register(), HttpStatus.CREATED)
+    }
+
+    @GetMapping
+    fun validate(token: Token): ResponseEntity<Token> {
+        val isValid = service.isValid(token)
+
+        return ResponseEntity(token, if (isValid) HttpStatus.OK else HttpStatus.NOT_FOUND)
+    }
+}
