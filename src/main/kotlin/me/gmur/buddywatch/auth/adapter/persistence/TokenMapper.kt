bcf337fe7a5f63e2367c5d830d33c74a3a22adc6
Jakub Gmur <jakub@gmur.me>
2020-11-06 00:16:59 +0100

    Map database ID to the domain object

--- src/main/kotlin/me/gmur/buddywatch/auth/adapter/persistence/TokenMapper.kt
+++ src/main/kotlin/me/gmur/buddywatch/auth/adapter/persistence/TokenMapper.kt
@@ -2,6 +2,7 @@ package me.gmur.buddywatch.auth.adapter.persistence
 
 import me.gmur.buddywatch.auth.domain.model.Token
 import me.gmur.buddywatch.commons.Mapper
+import me.gmur.buddywatch.jooq.tables.JToken.TOKEN
 import me.gmur.buddywatch.jooq.tables.records.JTokenRecord
 import org.springframework.stereotype.Component
 import java.util.UUID
@@ -12,9 +13,16 @@ class TokenMapper : Mapper<Token, JTokenRecord> {
     override fun mapToEntity(source: Token): JTokenRecord = mapToEntity(source, JTokenRecord())
 
     override fun mapToEntity(source: Token, base: JTokenRecord): JTokenRecord {
-        return base.apply {
+        val mapped = base.apply {
+            id = source.id
             token = source.toString()
         }
+
+        if (mapped.id == null) {
+            mapped.changed(TOKEN.ID, false)
+        }
+
+        return mapped
     }
 
     override fun mapToDomain(source: JTokenRecord): Token {
--- src/main/kotlin/me/gmur/buddywatch/auth/domain/model/Token.kt
+++ src/main/kotlin/me/gmur/buddywatch/auth/domain/model/Token.kt
@@ -3,7 +3,10 @@ package me.gmur.buddywatch.auth.domain.model
 import java.util.UUID
 import java.util.UUID.randomUUID
 
-data class Token(private val value: UUID = randomUUID()) {
+data class Token(
+    val value: UUID = randomUUID(),
+    val id: Long? = null
+) {
 
     override fun toString(): String {
         return value.toString()
