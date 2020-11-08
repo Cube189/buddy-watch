cfae0e654513f2dea4f8e7a30a249f6d73bde912
Jakub Gmur <jakub@gmur.me>
2020-11-08 21:40:00 +0100

    Refactor the mapper concept

--- src/main/kotlin/me/gmur/buddywatch/auth/adapter/persistence/PostgresTokenRepository.kt
+++ src/main/kotlin/me/gmur/buddywatch/auth/adapter/persistence/PostgresTokenRepository.kt
@@ -5,15 +5,13 @@ import me.gmur.buddywatch.auth.domain.port.TokenRepository
 import me.gmur.buddywatch.jooq.tables.JToken.TOKEN
 import org.jooq.DSLContext
 import org.springframework.stereotype.Repository
+import me.gmur.buddywatch.auth.adapter.persistence.TokenMapper as mapper
 
 @Repository
-class PostgresTokenRepository(
-    private val db: DSLContext,
-    private val mapper: TokenMapper
-) : TokenRepository {
+class PostgresTokenRepository(private val db: DSLContext) : TokenRepository {
 
     override fun store(token: Token): Token {
-        val record = mapper.mapToEntity(token, db.newRecord(TOKEN))
+        val record = mapper.mapToRecord(token, db.newRecord(TOKEN))
 
         record.store()
 
--- src/main/kotlin/me/gmur/buddywatch/auth/adapter/persistence/TokenMapper.kt
+++ src/main/kotlin/me/gmur/buddywatch/auth/adapter/persistence/TokenMapper.kt
@@ -1,18 +1,15 @@
 package me.gmur.buddywatch.auth.adapter.persistence
 
 import me.gmur.buddywatch.auth.domain.model.Token
-import me.gmur.buddywatch.commons.Mapper
 import me.gmur.buddywatch.jooq.tables.JToken.TOKEN
 import me.gmur.buddywatch.jooq.tables.records.JTokenRecord
-import org.springframework.stereotype.Component
 import java.util.UUID
 
-@Component
-class TokenMapper : Mapper<Token, JTokenRecord> {
+object TokenMapper {
 
-    override fun mapToEntity(source: Token): JTokenRecord = mapToEntity(source, JTokenRecord())
+    fun mapToRecord(source: Token): JTokenRecord = mapToRecord(source, JTokenRecord())
 
-    override fun mapToEntity(source: Token, base: JTokenRecord): JTokenRecord {
+    fun mapToRecord(source: Token, base: JTokenRecord): JTokenRecord {
         val mapped = base.apply {
             id = source.id
             token = source.toString()
@@ -30,7 +27,7 @@ class TokenMapper : Mapper<Token, JTokenRecord> {
         return mapped
     }
 
-    override fun mapToDomain(source: JTokenRecord): Token {
+    fun mapToDomain(source: JTokenRecord): Token {
         return Token(UUID.fromString(source.token), source.groupId, source.id)
     }
 }
