257b462f709e80370eca2cfff909855ad3ab64b6
Jakub Gmur <jakub@gmur.me>
2020-12-05 18:37:08 +0100

    Make REST endpoints plural

--- src/main/kotlin/me/gmur/buddywatch/group/adapter/rest/GroupEndpoints.kt
+++ src/main/kotlin/me/gmur/buddywatch/group/adapter/rest/GroupEndpoints.kt
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/group")
+@RequestMapping("/groups")
 class GroupEndpoints(
     private val createGroupUseCase: CreateGroupUseCase,
     private val assignMemberUseCase: AssignMemberUseCase,
