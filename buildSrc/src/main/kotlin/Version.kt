b21ca5c1ccb8370106d5f34b1fadd4f89002d8e1
Jakub Gmur <jakub@gmur.me>
2020-12-05 17:46:44 +0100

    Update dependencies

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -1,14 +1,14 @@
 object Version {
-    const val docker = "0.13.3"
-    const val flyway = "6.5.6"
+    const val docker = "0.13.4"
+    const val flyway = "7.3.0"
     const val gson = "2.8.6"
     const val java = "11"
-    const val jooq = "3.14.0"
-    const val kotest = "4.2.4"
-    const val kotlin = "1.3.72"
-    const val ktlint = "9.3.0"
-    const val micrometer = "1.5.5"
-    const val mockk = "1.10.0"
+    const val jooq = "3.14.4"
+    const val kotest = "4.3.1"
+    const val kotlin = "1.4.20"
+    const val ktlint = "9.4.1"
+    const val micrometer = "1.6.1"
+    const val mockk = "1.10.2"
     const val okhttp = "4.9.0"
     const val postgres = "42.2.16"
 
@@ -20,9 +20,9 @@ object Version {
         const val logback = "1.2.3"
     }
     object spring {
-        const val boot = "2.3.3.RELEASE"
+        const val boot = "2.4.0"
         const val dependencyManagement = "1.0.10.RELEASE"
-        const val security = "5.4.0"
+        const val security = "5.4.1"
     }
 }
 
