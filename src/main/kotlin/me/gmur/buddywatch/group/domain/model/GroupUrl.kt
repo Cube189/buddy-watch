241886acaf07b520f7d9d0bc78926af40919f356
Jakub Gmur <jakub@gmur.me>
2020-12-05 22:43:43 +0100

    Fix a bug with too long GroupUrl being generated

--- src/main/kotlin/me/gmur/buddywatch/group/domain/model/GroupUrl.kt
+++ src/main/kotlin/me/gmur/buddywatch/group/domain/model/GroupUrl.kt
@@ -2,7 +2,7 @@ package me.gmur.buddywatch.group.domain.model
 
 import org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric
 
-data class GroupUrl(private val url: String = (0..3).joinToString("-") { randomAlphanumeric(4) }) {
+data class GroupUrl(private val url: String = (0..2).joinToString("-") { randomAlphanumeric(4) }) {
 
     override fun toString(): String {
         return url
