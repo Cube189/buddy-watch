6bf993e76d8ddeca4496474b7aa7c915ec2c07f4
Jakub Gmur <jakub@gmur.me>
2020-12-05 23:51:33 +0100

    Put the region as a path variable in the REST API

--- src/main/kotlin/me/gmur/buddywatch/group/adapter/rest/ProviderEndpoints.kt
+++ src/main/kotlin/me/gmur/buddywatch/group/adapter/rest/ProviderEndpoints.kt
@@ -4,6 +4,7 @@ import me.gmur.buddywatch.group.domain.model.Provider
 import me.gmur.buddywatch.group.domain.port.ProvidersRepository
 import me.gmur.buddywatch.justwatch.api.Region
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -11,8 +12,8 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/providers")
 class ProviderEndpoints(private val providersRepository: ProvidersRepository) {
 
-    @GetMapping
-    fun forRegion(region: String): Set<Provider> {
+    @GetMapping("/{region}")
+    fun forRegion(@PathVariable region: String): Set<Provider> {
         // TODO: Map the passed-in region to an actual region
         val region = Region("en_US", "US", "United States")
 
