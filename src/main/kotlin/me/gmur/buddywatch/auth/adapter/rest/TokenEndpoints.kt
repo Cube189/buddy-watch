35b2e8e05e4c65ce10e299f04ef875d55d4c3c36
Jakub Gmur <jakub@gmur.me>
2020-12-05 21:37:59 +0100

    Cleanup code

--- src/main/kotlin/me/gmur/buddywatch/auth/adapter/rest/TokenEndpoints.kt
+++ src/main/kotlin/me/gmur/buddywatch/auth/adapter/rest/TokenEndpoints.kt
@@ -9,7 +9,6 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
@@ -39,4 +38,4 @@ class TokenEndpoints(private val repository: TokenRepository) {
 }
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-private class NoSuchTokenException(): ClientInputException()
+private class NoSuchTokenException : ClientInputException()
--- src/main/kotlin/me/gmur/buddywatch/common/domain/app/ClientInputException.kt
+++ src/main/kotlin/me/gmur/buddywatch/common/domain/app/ClientInputException.kt
@@ -1,5 +1,3 @@
 package me.gmur.buddywatch.common.domain.app
 
-import java.lang.RuntimeException
-
 open class ClientInputException() : RuntimeException()
