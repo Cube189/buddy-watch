705a6f9e833556998d0478e2eb4d8bea35e59e97
Jakub Gmur <jakub@gmur.me>
2020-12-24 23:02:07 +0100

    Change preference setting order

--- src/main/kotlin/me/gmur/buddywatch/recommendation/domain/app/SetFavoriteGenresUseCase.kt
+++ src/main/kotlin/me/gmur/buddywatch/recommendation/domain/app/SetFavoriteGenresUseCase.kt
@@ -1,20 +1,21 @@
 package me.gmur.buddywatch.recommendation.domain.app
 
 import me.gmur.buddywatch.group.domain.port.GroupRepository
+import me.gmur.buddywatch.recommendation.domain.model.taste.Actor
+import me.gmur.buddywatch.recommendation.domain.model.taste.Director
 import me.gmur.buddywatch.recommendation.domain.model.taste.command.SetFavoriteGenresCommand
-import me.gmur.buddywatch.recommendation.domain.port.ActorClient
+import me.gmur.buddywatch.recommendation.domain.port.DirectorClient
 import me.gmur.buddywatch.recommendation.domain.port.TasteRepository
 import org.springframework.stereotype.Service
-import me.gmur.buddywatch.recommendation.domain.model.taste.Actor
 
 @Service
 class SetFavoriteGenresUseCase(
     private val tasteRepository: TasteRepository,
     private val groupRepository: GroupRepository,
-    private val actorClient: ActorClient,
+    private val directorClient: DirectorClient,
 ) {
 
-    fun execute(command: SetFavoriteGenresCommand): Set<Actor> {
+    fun execute(command: SetFavoriteGenresCommand): Set<Director> {
         val token = command.token
         val genres = command.toGenresTaste()
         val decades = tasteRepository.getDecades(token)
@@ -23,6 +24,6 @@ class SetFavoriteGenresUseCase(
 
         val group = groupRepository.ofMember(token)
 
-        return actorClient.fetchFor(decades, genres, group)
+        return directorClient.fetchFor(decades, genres, group)
     }
 }
