9361338cecf7b062d27f7c1c52b6b3174e563891
Jakub Gmur <jakub@gmur.me>
2021-01-01 21:07:09 +0100

    Cache movies on startup

--- src/main/kotlin/me/gmur/buddywatch/recommendation/adapter/scheduled/MovieCachingJob.kt
+++ src/main/kotlin/me/gmur/buddywatch/recommendation/adapter/scheduled/MovieCachingJob.kt
@@ -1,14 +1,25 @@
 package me.gmur.buddywatch.recommendation.adapter.scheduled
 
 import me.gmur.buddywatch.recommendation.domain.app.FetchAllMoviesUseCase
+import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.context.ApplicationListener
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
 @Component
-class MovieCachingJob(private val fetchAllMoviesUseCase: FetchAllMoviesUseCase) {
+class MovieCachingJob(private val fetchAllMoviesUseCase: FetchAllMoviesUseCase) :
+    ApplicationListener<ApplicationReadyEvent> {
 
-    @Scheduled(cron = "0 0 6 * * FRI")
     fun execute() {
         fetchAllMoviesUseCase.execute()
+    }
+
+    @Scheduled(cron = "0 0 6 * * FRI")
+    fun scheduled() {
+        execute()
+    }
+
+    override fun onApplicationEvent(event: ApplicationReadyEvent) {
+        execute()
     }
 }
