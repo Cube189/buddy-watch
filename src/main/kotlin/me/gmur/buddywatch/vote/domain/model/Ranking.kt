327f997b3b5b5e26729435f90fcb4bd3d191a0f3
Jakub Gmur <jakub@gmur.me>
2021-01-20 20:31:48 +0100

    Fix a broken reference

--- src/main/kotlin/me/gmur/buddywatch/vote/domain/model/Ranking.kt
+++ src/main/kotlin/me/gmur/buddywatch/vote/domain/model/Ranking.kt
@@ -2,5 +2,5 @@ package me.gmur.buddywatch.vote.domain.model
 
 class Ranking(_votes: List<MovieVote>) {
 
-    val votes: List<MovieVote> = _votes.sortedByDescending { it.votes }
+    val votes: List<MovieVote> = _votes.sortedByDescending { it.voteCount }
 }
