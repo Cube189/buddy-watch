30757486db1349ec19a37ed763ff7c2a8a6d4fc1
Jakub Gmur <jakub@gmur.me>
2021-01-22 00:31:22 +0100

    Use DB data when returning available providers

--- src/main/kotlin/me/gmur/buddywatch/provider/adapter/rest/ProviderEndpoints.kt
+++ src/main/kotlin/me/gmur/buddywatch/provider/adapter/rest/ProviderEndpoints.kt
@@ -1,7 +1,7 @@
 package me.gmur.buddywatch.provider.adapter.rest
 
 import me.gmur.buddywatch.provider.domain.model.Provider
-import me.gmur.buddywatch.provider.domain.port.ProviderClient
+import me.gmur.buddywatch.provider.domain.port.ProviderRepository
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -9,12 +9,12 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/providers")
-class ProviderEndpoints(private val providerRepository: ProviderClient) {
+class ProviderEndpoints(private val providerRepository: ProviderRepository) {
 
     @GetMapping("/{region}")
-    fun forRegion(@PathVariable region: String): List<Provider> {
+    fun forRegion(@PathVariable region: String): Set<Provider> {
         // TODO: Map the passed-in region to an actual region
 
-        return providerRepository.fetchAll()
+        return providerRepository.all()
     }
 }
