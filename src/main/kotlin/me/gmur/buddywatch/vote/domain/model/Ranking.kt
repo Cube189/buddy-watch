2a783a97a9910c32efcba99aff4588a70836acb4
Jakub Gmur <jakub@gmur.me>
2021-01-22 00:36:26 +0100

    Tailor the data returned in the movie ranking

--- src/main/kotlin/me/gmur/buddywatch/vote/domain/model/Ranking.kt
+++ src/main/kotlin/me/gmur/buddywatch/vote/domain/model/Ranking.kt
@@ -3,4 +3,6 @@ package me.gmur.buddywatch.vote.domain.model
 class Ranking(_votes: List<MovieVote>) {
 
     val votes: List<MovieVote> = _votes.sortedByDescending { it.voteCount }
+        .filter { it.voteCount < 1 }
+        .take(10)
 }
